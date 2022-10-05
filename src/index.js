fb19158c356ffb3024d727e46b9d5ec39db78676
AlvaroCoder <papux45@gmail.com>
2022-10-04 23:36:09 -0500

    meh

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
-import App from './app';
-import './Assets/index.css'
+import App from './AppRoutes';
+import './Assets/index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<BrowserRouter>
-  <App></App>
+root.render(
+<BrowserRouter>
+            <App></App>
   </BrowserRouter>)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
